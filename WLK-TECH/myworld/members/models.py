f5dd4e63f8755aba5b0e58f5b743289e4fea74e7
wailinnkyaw03 <wailinnkyaw03@gmail.com>
2022-08-16 00:06:48 +0630

    a

--- WLK-TECH/myworld/members/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Members(models.Model):
-  username = models.CharField(max_length=255)
-  email = models.CharField(max_length=255)
-  comment = models.CharField(max_length=255)

1eb56662d2d6f1d115890bbfd4fec84ec81fb895
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2023-01-09 22:39:39 +0630

    a

--- hotelproject/assets/js/custom.js
+++ hotelproject/assets/js/custom.js
@@ -184,3 +184,42 @@ function closeForm() {
 	document.getElementById("myForm").style.display = "none";
 }
 
+//slick
+$(document).ready(function () {
+    $('.responsive').slick({
+        dots: true,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<i class="fas fa-angle-left left_arrow">',
+        nextArrow: '<i class="fas fa-angle-right right_arrow">',
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
+});

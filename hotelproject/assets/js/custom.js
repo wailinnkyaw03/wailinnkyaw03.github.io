3754af416faf6e3aa87c1a38237b0377d35fdc3f
wailinnkyaw03 <102975967+wailinnkyaw03@users.noreply.github.com>
2023-01-10 18:33:46 +0630

    a

--- hotelproject/assets/js/custom.js
+++ hotelproject/assets/js/custom.js
@@ -154,26 +154,74 @@ jQuery(function ($) {
 	AddReadMore();
  });
 
-//  Scroll to top
+
+
+//  Scroll to top for room.html
 // Get the button:
-let mybutton = document.getElementById("myBtn");
+let mybutton1 = document.getElementById("myBtn1");
 
 // When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {scrollFunction1()};
 
-function scrollFunction() {
+function scrollFunction1() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    mybutton1.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    mybutton1.style.display = "none";
   }
 }
 
 // When the user clicks on the button, scroll to the top of the document
-function topFunction() {
+function topFunction1() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
+
+//  Scroll to top for about.html
+// Get the button:
+let mybutton2 = document.getElementById("myBtn2");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction2()};
+
+function scrollFunction2() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton2.style.display = "block";
+  } else {
+    mybutton2.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction2() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+//  Scroll to top for service.html
+// Get the button:
+let mybutton3 = document.getElementById("myBtn3");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction3()};
+
+function scrollFunction3() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton3.style.display = "block";
+  } else {
+    mybutton3.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction3() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+
+
+
 
 // Popup CHat Window
 function openForm() {
